c22b50c9210e1b30d9612cc127f69148dd0ab53a
gabriele.salvato <gabriele.salvato@cnr.it>
2018-01-15 13:43:48 +0100

    work in progress

--- PODv2_0/PODv2_0.ino
+++ PODv2_0/PODv2_0.ino
@@ -24,11 +24,11 @@
 
 // MIDI Pin Out
 //        ------- 
-//      /    2    \
-//     /  5      4 \
-//     | 3        1 |
-//      \          /
-//       \ __**__ /
+//      /    2   \
+//     /  5     4 \
+//     | 3       1 |
+//      \         /
+//       \ __**_ /
         
 // Pin No.  IN Signal name  THRU Signal name  Out Signal name
 // 1        No Connect      No Connect        No Connect
@@ -53,6 +53,7 @@
 // connections between the two items of equipment (one for the audio, and one for the comms data).
 
 const byte MIDIvolume = 17;
+const byte MIDIwha    = 4;
 
 
 // Serial port communicates on digital pins 0 (RX) and 1 (TX) as well as with the computer via USB. 
@@ -86,19 +87,49 @@ const int d5 = 4;
 const int d6 = 3;
 const int d7 = 2;
 
+// PedalBoard - Arduino pins
+// Warning:
+// The Atmega datasheet cautions against switching analog pins in close
+// temporal proximity to making A/D readings (analogRead) on other analog pins.
+// This can cause electrical noise and introduce jitter in the analog system.
+//
+const int ChanA  = 6;
+const int ChanB  = 7;
+const int ChanC  = 8;
+const int ChanD  = 9;
+const int BankUp = A0;
+const int BankDn = A1;
+const int WhaSw  = 10;// Wha On-Off
+const int WhaSt  = 13;// Wha Status Led
+const int WhaVal = A2;
+const int Volume = A3;
+//
+// PedalBoard Status Values
+int statusChanA  = HIGH;
+int statusChanB  = HIGH;
+int statusChanC  = HIGH;
+int statusChanD  = HIGH;
+int statusBankUp = HIGH;
+int statusBankDn = HIGH;
+int statusWhaSw  = HIGH;// Wha On-Off
+int statusWhaSt  = HIGH;// Wha Status Led
+int statusWhaVal = 0;
+int statusVolume = 0;
+int currentBank  = 0;
+int currentChan  = 1;
+
+
 LiquidCrystal LCD(rs, en, d4, d5, d6, d7);
 
 
 MIDI_CREATE_DEFAULT_INSTANCE();
-byte    podAddress     = 1;// This is the MIDI address #1 (the POD default)                                               
-
+byte podAddress     = 1;// This is the MIDI address #1 (the POD default)
+int  input;                                                 
 
 // This function will be automatically called when a
 // ControlChange is received.
 void
 handleControlChange(byte channel, byte number, byte value) {
-    if(channel != podAddress)
-        return;// We don't act as MIDI thru
     if(number == MIDIvolume) {// Channel Volume
         LCD.setCursor(5, 1);
         LCD.print("   ");
@@ -121,8 +152,6 @@ handleControlChange(byte channel, byte number, byte value) {
 // ProgramChange is received.
 void
 handleProgramChange(byte channel, byte number) {
-    if(channel != podAddress)
-        return;// We don't act as MIDI thru
     LCD.setCursor(5, 0);
     LCD.print("   ");
     LCD.setCursor(5, 0);
@@ -138,11 +167,11 @@ handleSysEx(byte *array, unsigned size) {
     LCD.print(size);
 }
 
+
 void 
-setup() {
+setupLCD() {
     // set up the LCD
     LCD.begin(20, 4);// Number of Columns and Rows:
-    
     // Print the headers to the LCD. 
     LCD.setCursor(0, 0);// LCD.setCursor(col, row)   
     LCD.print("Pgm:");
@@ -152,21 +181,121 @@ setup() {
     LCD.print("CCn:");
     LCD.setCursor(10, 2);
     LCD.print("Val:");
-    
-    // Connect the handleControlChange function to the library.
-    // It will be called upon reception of a ControlChange.
+}
+
+
+void
+setupPedalBoard() {
+    // There are 20K pullup resistors built into the Atmega chip... 
+    pinMode(ChanA,  INPUT_PULLUP);// Channel A  Switch
+    pinMode(ChanB,  INPUT_PULLUP);// Channel B  Switch
+    pinMode(ChanC,  INPUT_PULLUP);// Channel C  Switch
+    pinMode(ChanD,  INPUT_PULLUP);// Channel D  Switch
+    pinMode(BankUp, INPUT_PULLUP);// Bank Up    Switch
+    pinMode(BankDn, INPUT_PULLUP);// Bank Down  Switch
+    pinMode(WhaSw,  INPUT_PULLUP);// Wha On-Off Switch
+    pinMode(WhaSt,  OUTPUT);      // Wha Status Led (same pin as the internal Led)
+    pinMode(WhaVal, INPUT);       // Wha Potentiometer
+    pinMode(Volume, INPUT);       // Volume Potentiometer
+    statusVolume = analogRead(Volume >> 3);
+    statusWhaVal = analogRead(WhaVal >> 3);
+}
+
+
+void
+setupMIDI() {
     MIDI.setHandleControlChange(handleControlChange);
-    // Connect the handleProgramChange function to the library.
-    // It will be called upon reception of a ProgramChange.
     MIDI.setHandleProgramChange(handleProgramChange);
     MIDI.setHandleSystemExclusive(handleSysEx);
-    MIDI.begin();
+    MIDI.begin(podAddress);
+  
+}
+
+
+void 
+setup() {
+    setupLCD();
+    setupPedalBoard();
+    setupMIDI();
 }
 
 
 void 
 loop() {
-    // Call MIDI.read the fastest you can
-    // for real-time performance.
+    // Channel Switches
+    input = digitalRead(ChanA);
+    if(input != statusChanA) {
+        statusChanA = input;
+        if(input == LOW) {
+            currentChan = 1;
+            MIDI.sendProgramChange(currentBank+currentChan, podAddress);
+        }
+    }
+    input = digitalRead(ChanB);
+    if(input != statusChanB) {
+        statusChanB = input;
+        if(input == LOW) {
+            currentChan = 2;
+            MIDI.sendProgramChange(currentBank+currentChan, podAddress);
+        }
+    }
+    input = digitalRead(ChanC);
+    if(input != statusChanC) {
+        statusChanC = input;
+        if(input == LOW) {
+            currentChan = 3;
+            MIDI.sendProgramChange(currentBank+currentChan, podAddress);
+        }
+    }
+    input = digitalRead(ChanD);
+    if(input != statusChanD) {
+        statusChanD = input;
+        if(input == LOW) {
+            currentChan = 4;
+            MIDI.sendProgramChange(currentBank+currentChan, podAddress);
+        }
+    }
+    // Bank Switches
+    input = digitalRead(BankDn);
+    if(input != statusBankDn) {
+        statusBankDn = input;
+        if(input == LOW) {
+            currentBank += 4;
+            if(currentBank > 36)
+                currentBank = 0;
+            MIDI.sendProgramChange(currentBank+currentChan, podAddress);
+        }
+    }
+    input = digitalRead(BankUp);
+    if(input != statusBankUp) {
+        statusBankUp = input;
+        if(input == LOW) {
+            currentBank -= 4;
+            if(currentBank < 0)
+                currentBank = 32;
+            MIDI.sendProgramChange(currentBank+currentChan, podAddress);
+        }
+    }
+    // Wha
+    input = digitalRead(WhaSw);
+    if(input != statusWhaSw) {
+        statusWhaSw = input;
+        if(input == LOW) {
+            if(statusWhaSt == HIGH)
+                statusWhaSt = LOW;
+            else
+                statusWhaSt = HIGH;
+            digitalWrite(WhaSt, statusWhaSt);
+        }
+    }
+    if(statusWhaSw == HIGH) {
+        input = analogRead(WhaVal >> 3);
+        if(input != statusWhaVal) {
+            statusWhaVal = input;
+            MIDI.sendControlChange(MIDIwha, statusWhaVal, podAddress);
+        }
+    }
+
+    // Call MIDI.read() the fastest you can for real-time performance.
     MIDI.read();
 }
