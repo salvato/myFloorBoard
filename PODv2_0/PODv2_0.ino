76f94f519c3aa602ec6f459f1623c222fdcd9491
gabriele.salvato <gabriele.salvato@cnr.it>
2018-01-18 16:29:43 +0100

    work in progress

--- PODv2_0/PODv2_0.ino
+++ PODv2_0/PODv2_0.ino
@@ -67,15 +67,16 @@
 
 //The circuit:(ATTENTION max contrast is when VO is ground !)
 
-// * LCD RS pin to digital pin 12
-// * LCD Enable pin to digital pin 11
-// * LCD D4 pin to digital pin 5
-// * LCD D5 pin to digital pin 4
-// * LCD D6 pin to digital pin 3
-// * LCD D7 pin to digital pin 2
-// * LCD R/W pin to ground
-// * LCD VSS pin to ground
-// * LCD VCC pin to 5V
+// * LCD RS pin     to digital pin 7
+// * LCD Enable pin to digital pin 6
+// * LCD D4 pin     to digital pin 5
+// * LCD D5 pin     to digital pin 4
+// * LCD D6 pin     to digital pin 3
+// * LCD D7 pin     to digital pin 2
+// * LCD R/W pin    to ground
+// * LCD VSS pin    to ground
+// * LCD VCC pin    to 5V
+//
 // * 10K potentiometer:
 // *   ends to +5V and ground
 // *   wiper to LCD VO pin (pin 3)
