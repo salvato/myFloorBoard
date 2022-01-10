d7c164e66b964dc04bf85473fea6c78031ff5591
Gabriele Salvato <gabriele.salvato@cnr.it>
2022-01-10 13:08:27 +0100

    Inserita la licenza MIT

--- PODv2_0/PODv2_0.ino
+++ PODv2_0/PODv2_0.ino
@@ -1,3 +1,25 @@
+//MIT License
+
+//Copyright (c) 2018 salvato
+
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+
+//The above copyright notice and this permission notice shall be included in all
+//copies or substantial portions of the Software.
+
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//SOFTWARE.
+
 // Midi Message Format:
 
 // Within the MIDI Specification there are two basic types of message bytes: the STATUS byte and the DATA byte. 
@@ -134,9 +156,9 @@ const unsigned long debounceTime = 20; // milliseconds
 unsigned long now;
 
 
-                                              //////////////////
-                                              // Used variables
-                                              //////////////////
+                          //////////////////
+                          // Used variables
+                          //////////////////
 volatile bool bPODready = false;
 int input; 
                                                 
@@ -167,9 +189,9 @@ static InputDebounce buttonWha;
 MIDI_CREATE_DEFAULT_INSTANCE();
 
 
-                                              /////////////////////////
-                                              // Button Event Handlers
-                                              /////////////////////////
+                          /////////////////////////
+                          // Button Event Handlers
+                          /////////////////////////
 void
 BankUp_pressedCallback(uint8_t pinIn) {
     currentBank++;
@@ -258,9 +280,9 @@ WhaSw_pressedCallback(uint8_t pinIn) {
 //}
 
 
-                                            ////////////////////////
-                                            // MIDI events handlers
-                                            ////////////////////////
+                    ////////////////////////
+                    // MIDI events handlers
+                    ////////////////////////
 void
 handleControlChange(byte channel, byte number, byte value) {
     bPODready = true;
